4547e46b701323be806d5b1eeb26867497b542ba
Venugopala Kota <venukota@hotmail.com>
2020-03-16 13:39:47 +0530

    Annotations added to Person

--- SimpliPromo/Entities/Person.cs
+++ SimpliPromo/Entities/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace SimpliPromo.Entities
     {
         [DatabaseGeneratedAttribute(DatabaseGeneratedOption.None)]
         public int PersonId { get; set; }
+        [Required]
         public string FirstName { get; set; }
         public string LastName { get; set; }
         [NotMapped]
